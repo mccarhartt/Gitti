08c9e69eba8ca3ae66c00355b0148acf080694b2
Daniil <dshirpakov@gmail.com>
2026-09-06 19:27:14 +0300

    2

--- peliprojekti/peli.py
+++ peliprojekti/peli.py
@@ -1,5 +1,29 @@
 #Tee kansioon ohjelma, joka kysyy pelaajan nimen ja iän, tallentaa nämä muuttujiin ja tulostaa konsoliin.
 nimi=input("Sinun nimi: \n")
-ika=input("Sinun ikä: \n")
-print(f"Nimesi on {nimi}")
-print(f"Sinun ikä on {ika}")
+ika=int(input("Sinun ikä: \n"))
+
+if ika < 12:
+    print("Olet liian nuori")
+else:
+    print("Tervetuloa!")
+
+    while True:
+        print("PÄÄVALIKKO")
+        print("1")
+        print("2")
+        print("3")
+        print("lopeta")
+
+        komento = input("komento : ")
+
+        if komento == "1":
+            print("start")
+        elif komento == "2":
+            print("pause")
+        elif komento == "3":
+            print("asetukset")
+        elif komento == "lopeta":
+            print("Lopetetaan")
+            break
+
+
